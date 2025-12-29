0435bcfe860fae66b635ad110bd5651f8cd36958
BENNNZO <benjamin.phillips.webdev@gmail.com>
2025-12-28 19:30:03 -0500

    Update colors.

--- src/components/files/Directory.jsx
+++ src/components/files/Directory.jsx
@@ -5,7 +5,7 @@ export default function Directory({ directory, path }) {
   return (
     <Link
       href={`${path ? `/files/${path.join('/')}` : '/files'}/${encodeURIComponent(directory.name)}`}
-      className={`flex gap-2 items-center px-3 py-2 shadow-md bg-zinc-900 rounded-lg hover:bg-zinc-800 duration-75 ${directory.hidden ? 'opacity-50' : ''}`}
+      className={`flex gap-2 items-center px-3 py-2 shadow-md rounded-lg duration-75 ${directory.hidden ? 'bg-zinc-900/50 hover:bg-zinc-800/50' : 'bg-zinc-900 hover:bg-zinc-800'}`}
     >
       <Icon name="folder-fill" size={6} />
       <p className="whitespace-nowrap text-ellipsis overflow-hidden">{directory.name}</p>
